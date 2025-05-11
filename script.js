6b2e44098d521e538d5d7be5505e8f1ab34f7bea
mariusboncev1 <mariusboncev1@gmail.com>
2025-05-11 16:15:44 +0300

    button layout & design, starting of the backend

---
+++ script.js
@@ -0,0 +1,41 @@
+let currentLetter = 'A';
+let isRunning = false;
+let lastUpdateTime = 0;
+let letterColor;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  
+  startButton = createButton('START');
+  startButton.position(width/2 - 60, height/2 + 30);
+  startButton.style('background-color', '#4CAF50');
+  startButton.style('color', 'white');
+  startButton.mousePressed(startSecventa);
+  
+  stopButton = createButton('STOP');
+  stopButton.position(width/2 + 10, height/2 + 30);
+  stopButton.style('background-color', '#f44336');
+  stopButton.style('color', 'white');
+  stopButton.mousePressed(stopSecventa);
+  
+//   letterColor = randomColor();
+}
+
+
+
+function draw() {
+  background(240);
+  
+  fill(letterColor);
+  text(currentLetter, width/2, height/2 - 30);
+  
+  if (isRunning && millis() - lastUpdateTime > 500) {
+    updateLetter();
+    lastUpdateTime = millis();
+  }
+}
+
+
+
